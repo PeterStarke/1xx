d6cc50dc0e2de3fd49f1d19f2037c0c077989531
peterstarke <pstarke@racc.edu>
2018-03-11 15:35:10 -0400

    added menu builder function to recusivly build the mune and sub-menu itmes. We will complete the styling in the next branch.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,53 +1,73 @@
 // JavaScript Document
 
+window.onload = init();
+
 function init() {
 
-    window.addEventListener('scroll', function (e) {
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 300,
-            header = document.querySelector("header");
-        if (distanceY > shrinkOn) {
-            classie.add(header, "smaller");
-        } else {
-            if (classie.has(header, "smaller")) {
-                classie.remove(header, "smaller");
-            }
-        }
-    });
+	window.addEventListener('scroll', function (e) {
+		var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+			shrinkOn = 300,
+			header = document.querySelector("header");
+		if (distanceY > shrinkOn) {
+			classie.add(header, "smaller");
+		} else {
+			if (classie.has(header, "smaller")) {
+				classie.remove(header, "smaller");
+			}
+		}
+	});
 
 
-    $.ajax({
-        method: 'GET',
-        url: 'assets/data/menu.json',
-        dataType: 'json',
-        success: function (data) {
-            
-            console.log('all good');
-            console.log(data.menu.length);
-            console.log(data.menu);
+	$.ajax({
+		method: 'GET',
+		url: 'assets/data/menu.json',
+		dataType: 'json',
+		success: function (data) {
 
-            if (data.menu.length > 0) {
+			var menu = menuBuilder(data.menu);
 
-                data.menu.forEach(function (data) {
+			$('nav').append(menu);
 
-                    console.log(data.MenuName);
-                    console.log(data.MenuLink);
+		},
+		error: function () {
 
-                    $('nav').append('<a href="' + data.MenuLink + '">' + data.MenuName + '</a>');
+			console.log('all is not good');
 
-                });
-            }
-
-        },
-        error: function () {
-            console.log('all is not good');
-        }
-    });
-
-
+		}
+	});
 
 }
 
 
+function menuBuilder(obj) {
 
-window.onload = init();
+	var theMenu = '';
+
+	if (obj.length > 0) {
+
+		theMenu = theMenu + '<ul>';
+
+		obj.forEach(function (item) {
+
+			theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+
+			if (item.Menus.length > 0) {
+
+				theMenu = theMenu + menuBuilder(item.Menus);
+
+			}
+
+			theMenu = theMenu + '</li>';
+
+		});
+
+		theMenu = theMenu + '</ul>';
+
+	} else {
+
+		console.log('no data');
+
+	}
+
+	return theMenu;
+}
