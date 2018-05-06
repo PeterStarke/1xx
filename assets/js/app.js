d06269b2764a21d66a6d8268bffb678370155619
peterstarke <pstarke@racc.edu>
2018-05-05 20:53:03 -0400

    105/106 challenge for adding General Links and Latest Posts

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,23 +37,60 @@ function init() {
 }
 
 
-unction menuBuilder(obj) {
-    var theMenu = '';
-    if (obj.length > 0) {
-        theMenu = theMenu + '<ul>';
-        obj.forEach(function(item) {
-            theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
-            if (item.children) {
-                theMenu = theMenu + menuBuilder(item.children);
-            }
-            theMenu = theMenu + '</li>';
-        });
-        theMenu = theMenu + '</ul>';
-    } else {
-        console.log('no data');
-    }
-    return theMenu;
+
+
+$.ajax({
+		method: 'GET',
+		url: 'http://me.mrstarke.com/me/wp-json/wp-api-menus/v2/menus/3',
+		dataType: 'json',
+		success: function (data) {
+			var menu = menuBuilder(data.items, 'genLinks', 'footer-ul');
+			$('#genLinks').replaceWith(menu);
+			$('#genLinks li a').click(function () {
+				getPage($(this).data("pgid"));
+			});
+
+
+		},
+		error: function () {
+			console.log('all is not good');
+		}
+	});
+	getPosts();
 }
+
+function menuBuilder(obj, elID, elClassName) {
+
+	var theMenu = '';
+
+	if (obj) {
+		var inElId = (elID !== undefined) ? ' id="' + elID + '"' : '';
+
+		var inLineClass = '';
+		if (elClassName !== undefined) {
+			inLineClass = ' class="' + elClassName + '"';
+
+		}
+		theMenu = theMenu + '<ul' + inElId + '' + inLineClass + '>';
+		obj.forEach(function (item) {
+			theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
+			if (item.children) {
+				theMenu = theMenu + menuBuilder(item.children);
+			}
+			theMenu = theMenu + '</li>';
+		});
+		theMenu = theMenu + '</ul>';
+	} else {
+		console.log('no data')
+	}
+	return theMenu;
+
+}
+
+
+
+
+
 
 
 function getPage(obj) {
@@ -80,4 +117,32 @@ function getPage(obj) {
             console.log('bad');
         }
     });
+}
+
+
+
+
+
+function getPosts() {
+	$("#footerPosts").html('<p id="postLdr"><i class="fa fa-cogs"></i> Loading Posts</p>');
+	$.ajax({
+		method: 'GET',
+		url: 'http://me.mrstarke.com/me/wp-json/wp/v2/posts?orderby=date&order=asc&per_page=5',
+		dataType: 'json',
+		success: function (data) {
+			console.log('inside success');
+			console.log(data);
+			$("#footerPosts").html('');
+			data.forEach(function (item) {
+				$("#footerPosts").append('<p>'+ item.title.rendered + item.date + item.time + ' <span>August 3,2015</span></p>');
+
+				console.log(item);
+				console.log(item.slug + ' - ' + item.date + ' - ' + item.title.rendered);
+
+			});
+		},
+		error: function () {
+			console.log('bad');
+		}
+	});
 }
