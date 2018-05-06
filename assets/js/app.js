2c16fa9994ddfc345b7cc3fc78c3841ee0723433
peterstarke <pstarke@racc.edu>
2018-05-05 20:10:06 -0400

    added click event funtions and listners. Added preloader for home  page on initial load. Updated the links in the menu and logo to call page content from our wordpress api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
-// JavaScript Document
-
+// inside out project STEP-105
 window.onload = init();
 
 function init() {
 
-    window.addEventListener('scroll', function (e) {
+    window.addEventListener('scroll', function(e) {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 300,
             header = document.querySelector("header");
@@ -17,61 +16,68 @@ function init() {
         }
     });
 
-
-    $.ajax({
+   $.ajax({
         method: 'GET',
         url: 'http://me.mrstarke.com/me/wp-json/wp-api-menus/v2/menus/2',
         dataType: 'json',
-        success: function (data) {
-            
+        success: function(data) {
             $('nav').hide();
-            
             var menu = menuBuilder(data.items);
-
             $('nav').html(menu).slideDown();
-            
+            $('nav li a').click(function() {
+                getPage($(this).data("pgid"));
+            });
+            getPage(314);
             $("#loaderDiv").fadeOut("slow");
-
         },
-        error: function () {
-
+        error: function() {
             console.log('all is not good');
-
         }
     });
-
 }
 
 
-function menuBuilder(obj) {
-
+unction menuBuilder(obj) {
     var theMenu = '';
-
     if (obj.length > 0) {
-
         theMenu = theMenu + '<ul>';
-
-        obj.forEach(function (item) {
-
-            theMenu = theMenu + '<li><a href="#">' + item.title + '</a>';
-
+        obj.forEach(function(item) {
+            theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
             if (item.children) {
-
                 theMenu = theMenu + menuBuilder(item.children);
-
             }
-
             theMenu = theMenu + '</li>';
-
         });
-
         theMenu = theMenu + '</ul>';
-
     } else {
-
         console.log('no data');
-
     }
-
     return theMenu;
+}
+
+
+function getPage(obj) {
+    $("#loaderDiv").fadeIn("slow");
+    $.ajax({
+        method: 'GET',
+        url: 'http://me.mrstarke.com/me/wp-json/wp/v2/pages/' + obj,
+        dataType: 'json',
+        success: function(data) {
+            var pgbuild = '';
+            pgbuild = '<section><div class="container">' + data.content.rendered + '</div></section>';
+            $("#content").fadeOut(function() {
+                $('html').animate({
+                    scrollTop: 0
+                }, 'slow'); //IE, FF
+                $('body').animate({
+                    scrollTop: 0
+                }, 'slow'); 
+                $(this).html(pgbuild).fadeIn();
+                $("#loaderDiv").fadeOut("slow");
+            });
+        },
+        error: function() {
+            console.log('bad');
+        }
+    });
 }
